b58da37aba0c9f20c422ffc30e854314e4c3bbc5
Niclas <niclas.rogulski@gmail.com>
2025-04-08 11:43:27 +1000

    Basic elevator room and untested room code.

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -3,18 +3,43 @@ using UnityEngine;
 
 public class Room : Area
 {
+    
     private void Start()
     {
+        Connector startConnector = null;
+        var isComplete = false;
+        
         connectors.ToList().ForEach(connector =>
         {
-            connector.OnPlayerEnter += area =>
+            var closeConnector = connector.connection;
+            var hallway = closeConnector.area;
+            var farConnector = hallway.GetOppositeConnector(closeConnector);
+            
+            closeConnector.OnPlayerExit += area =>
             {
+                if (!area.Equals(hallway)) return;
                 
+                if (startConnector && !isComplete && !startConnector.Equals(farConnector))
+                {
+                    Debug.Log("LEAVING ROOM: Finishing room (opposite side of start)");
+                    isComplete = true;
+                }
             };
 
-            connector.OnPlayerExit += area =>
+            farConnector.OnPlayerExit += area =>
             {
-                
+                if (area.Equals(hallway)) return;
+
+                if (!startConnector)
+                {
+                    Debug.Log("ENTERING ROOM: Starting room");
+                    startConnector = closeConnector;
+                }
+                else if (!isComplete && startConnector.Equals(farConnector))
+                {
+                    Debug.Log("LEAVING ROOM: Finishing room (same side as start)");
+                    isComplete = true;
+                }
             };
         });
     }
